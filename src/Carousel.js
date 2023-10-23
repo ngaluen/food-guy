47ebf6f1d19c8dfd577d0edce705f45cfa8a1582
ngaluen <ngawunhui.hk@gmail.com>
2023-10-23 16:56:01 +0800

    fix image test 2

--- src/Carousel.js
+++ src/Carousel.js
@@ -86,7 +86,7 @@ export default function Carousel() {
                 }}
               ></div>
               <img
-                href={process.env.PUBLIC_URL + img.src}
+                src={img.src}
                 alt={img.name}
                 style={{
                   width: "600px",
